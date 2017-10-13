c240dae04fd484c3c2e2c8a846800d48f69c736b
Trainee <Trainee@ET-PAY.everteam-mea.com>
2017-10-13 09:25:54 +0300

    this is a message

---
+++ ThisIsAJavaProject/src/thisIsaProject/ThisIsAClassss.java
@@ -0,0 +1,20 @@
+package thisIsaProject;
+
+import java.util.Scanner;
+
+public class ThisIsAClassss {
+	public static void main(String args[]){  
+	
+	Scanner sc=new Scanner(System.in);  
+    
+	  System.out.println("Enter your roll no");
+	   int rollno=sc.nextInt();  
+	   System.out.println("Enter your name");  
+	   String name =sc.next();  
+	   System.out.println("Enter your fee");  
+	   double fee=sc.nextDouble();  
+	   System.out.println("Rollno:"+rollno+" name:"+name+" fee:"+fee);;  
+	   sc.close();  
+	}
+}
+
